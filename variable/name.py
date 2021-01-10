d1499455d297da04023a1e3f0be5dde67d8b8847
taratep123 <taratep.tr@ku.th>
2021-01-10 14:35:04 +0700

    no message

---
+++ variable/name.py
@@ -0,0 +1,12 @@
+#สามารถประกาศได้
+myvar = "tiger"
+my_var = "tiger"
+_my_var = "tiger"
+myVar = "tiger"
+MYVAR = "tiger"
+myvar2 = "tiger"
+
+#ไม่สามารถประกาศได้
+2myvar = "tiger"
+my-var = "tiger"
+my.var = "tiger"
