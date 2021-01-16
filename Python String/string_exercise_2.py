ce363a81ffd6cc650a2ac64aa3aeeaae5603a2f7
taratep123 <taratep.tr@ku.th>
2021-01-16 15:45:53 +0700

    exercise_2

---
+++ Python String/string_exercise_2.py
@@ -0,0 +1,26 @@
+#จงเขียนคำสั่งเพื่อแสดงความยาวของตัวอักษร "I am the best programmer"
+
+#จงเขียนคำสั่งเพื่อแสดงอักษรแรกของข้อความ "I am the best programmer"
+
+#จงเขียนคำสั่งเพื่อแสดง "best" ของข้อความ "I am the best programmer"
+
+#จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ที่ไม่มี space
+
+#จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ให้เป็นตัวพิมใหญ่ทั้งหมด
+
+#จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ให้เป็นตัวพิมเล็กทั้งหมด
+
+#จงเขียนคำสั่งเพื่อแสดข้อความ "I am the best programmer" ที่ถูกแทนที่อักษร e ด้วย z ทั้งหมด
+
+#จงเติมคำในช่องว่าเพื่อแสดงชื่อ
+myname = "tiger"
+txt = "i am  the best programmer"
+print(myname)
+print(txt.format(myname))
+print(len(txt))
+print(txt[0])
+print(txt[9:14])
+print(txt.replace(" " , " "))
+print(txt.upper())
+print(txt.lower())
+print(txt.replace("e", "z"))
