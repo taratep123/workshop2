51446c80350203e547b40613a046ee40479b39bb
taratep123 <taratep.tr@ku.th>
2021-01-17 15:37:41 +0700

    complete Python Set ,Tuples And Exercise_set_2

---
+++ Python Sets/Access Set items.py
@@ -0,0 +1,12 @@
+#EX1
+thisset = {"apple" , "banana", "cherry"}
+
+for x in thisset:
+    print(x)
+
+
+
+#EX2
+fruits = {"apple" ,"banana" ,"cherry"}
+if "apple" in fruits
+    print("Yes, apple is a fruit!")
